5c2ed46df643213586c8327164adb830326f7e0a
felixreichenbach <felix.reichenbach@mongodb.com>
2020-04-26 16:54:35 +0200

    init

---
+++ Farms2/Farms2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Farms2
+//
+//  Created by Felix Reichenbach on 26.04.20.
+//  Copyright © 2020 Felix Reichenbach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

91b88907d3900e46d87ad7794cc2726b553c378b
felixreichenbach <felix.reichenbach@mongodb.com>
2020-10-04 22:41:56 +0200

    cleaned up login/logout procedure

--- Farms2-web/javascript2.js
+++ Farms2-web/javascript2.js
@@ -2,14 +2,13 @@
 // Retrieve App ID from URL
 var hostname = $(location).attr('hostname');
 var res = hostname.split(".");
-console.log(res[0])
+//console.log(res[0])
 
 // Initialize Realm App client with App ID retrieved from URL
-
 const app = new Realm.App({ id: res[0] });
 
 async function loginEmailPassword(email, password) {
-    // Create an credential
+    // Create a credential
     const credentials = Realm.Credentials.emailPassword(email, password);
 
     try {
@@ -19,45 +18,47 @@ async function loginEmailPassword(email, password) {
         console.assert(user.id === app.currentUser.id)
         return user
     } catch (err) {
-        console.error("Failed to log in", err);
+        //console.error("Failed to log in", err);
     }
 }
 
 $(document).ready(function () {
-    /*
-    console.log(app.currentUser.id);
-    if (app.currentUser.id) {
+
+    // Check if a user is logged in
+    if (app.currentUser) {
         $('.form-signin').hide();
         $('#orderForm').show();
     }
-    */
-    // Add order item
-    const orderItem = {"name":"Greetings from Farms 2 Web Client"}
-    $("#addButton").click(async function () {
-        const result = await app.functions.newOrder(orderItem);
-    });
 
     // Login User
-
     $("#loginForm").submit(function (event) {
         var email = $("#loginEmail").val();
         var password = $("#password").val();
         event.preventDefault();
 
-        loginEmailPassword(email, password)
-            .then(user => {
-                if (user == undefined) {
-                    console.log("Login Failed", user)
-                } else {
-                    console.log("Successfully logged in!", user)
-                    $('.form-signin').hide();
-                    $('#orderForm').show();
-                }
-            })
+        loginEmailPassword(email, password).then(user => {
+            if (app.currentUser) {
+                console.log("Successfully logged in!", user)
+                $('#loginError').text('');
+                $('.form-signin').hide();
+                $('#orderForm').show();
+            } else {
+                $('#loginError').text('Login failed!');
+            }
+        });
     });
 
-    $('#logout').click(async function() {
-        //stub
+    // Logout current user
+    $('#logout').click(async function () {
+        console.log("logout: ", app.currentUser._profile.email)
+        await app.currentUser.logOut();
+        $('.form-signin').show();
+        $('#orderForm').hide();
     })
-});
 
+    // Add order item
+    const orderItem = { "name": "Greetings from Farms 2 Web Client" }
+    $("#addButton").click(async function () {
+        const result = await app.functions.newOrder(orderItem);
+    });
+});
